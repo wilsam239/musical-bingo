9b8fb4672b4677385bfaf79e589ddec9616fa2f8
Sam Williamson <samw@farmlab.com.au>
2023-06-20 21:07:37 +1000

    Added a version controller

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import { APP_VERSION } from './assets/version';
+
+const version = APP_VERSION
+
+console.log(`Running version: ${version}`)
 </script>
 
 <template>
