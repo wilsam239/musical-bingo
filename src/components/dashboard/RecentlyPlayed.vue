f182c8a409091602de2c322a71a1bbff0826e871
Sam Williamson <samw@farmlab.com.au>
2023-07-05 21:09:18 +1000

    Updated the styling for recently played songs

--- src/components/dashboard/RecentlyPlayed.vue
+++ src/components/dashboard/RecentlyPlayed.vue
@@ -5,9 +5,32 @@ import { defineComponent, onMounted, ref } from 'vue';
 const spotify = SpotifyService;
 
 const songsToShow = ref(spotify.sessionPlayed);
+const songFilter = ref('');
 </script>
 <template>
-  <div v-for="song of songsToShow" v-bind:key="song.id">
-    {{ song.name }}
-  </div>
+  <q-input square outlined v-model="songFilter" label="Filter Songs" />
+  <q-list bordered class="rounded-borders">
+    <q-item v-for="playlist of songsToShow" v-bind:key="playlist.id">
+      <q-avatar rounded class="q-mr-md"> </q-avatar>
+
+      <q-item-section>
+        <q-item-label lines="1">
+          <span class="text-weight-medium">
+            <q-tooltip>{{ playlist.name }}</q-tooltip>
+            {{ playlist.name }}
+          </span>
+        </q-item-label>
+        <q-item-label lines="1">
+          <span class="text-grey-8">
+            <q-tooltip>
+              {{ playlist.artists.map((a) => a.name).join(', ') }}
+            </q-tooltip>
+            {{ playlist.artists.map((a) => a.name).join(', ') }}
+          </span>
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-separator spaced />
+  </q-list>
 </template>
