2b69c9f80b2332b4efd1ae62064eeb66075ff8d9
Sam Williamson <samw@farmlab.com.au>
2023-07-10 13:07:48 +1000

    Updated quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: 'musical-bingo',
       // analyze: true,
       // env: {},
       // rawDefine: {}
