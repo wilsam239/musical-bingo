52063120d38a75ae2884275f5ceeda2d490b9725
Sam Williamson <samw@farmlab.com.au>
2023-07-10 10:13:25 +1000

    Prevent negative values

--- src/components/dashboard/PlaybackUpdater.vue
+++ src/components/dashboard/PlaybackUpdater.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="playback-update-container">
     <div class="spaced-inputs">
-      <q-input square outlined v-model="playbackTimer" label="Song Timer" />
-      <q-input square outlined v-model="scrubber" label="Position In Song" />
+      <q-input
+        square
+        outlined
+        v-model="playbackTimer"
+        label="Song Timer"
+        type="number"
+        min="0"
+        oninput="this.value = 
+ !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null"
+      />
+      <q-input
+        square
+        outlined
+        v-model="scrubber"
+        label="Position In Song"
+        type="number"
+      />
       <q-input
         square
         outlined
         v-model="pauseTimer"
         label="Time between songs"
+        type="number"
+        min="0"
+        oninput="this.value = 
+ !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null"
       />
     </div>
     <div class="row justify-center q-mb-sm">
-      <q-btn @click="update(true)" color="primary">Update!</q-btn>
+      <q-btn
+        :disable="isDisabled()"
+        @click="update(scrubber, playbackTimer, pauseTimer, true)"
+        color="primary"
+        >Update!</q-btn
+      >
     </div>
   </div>
 </template>
@@ -31,22 +55,32 @@ const pauseTimer = ref(0);
 
 let playbackSub: Subscription | undefined = undefined;
 
-function pause() {
-  if (pauseTimer.value > 0) {
-    return SpotifyService.pause().pipe(delay(pauseTimer.value * 1000));
+function isDisabled() {
+  if (scrubber.value < 0 && Math.abs(scrubber.value) < playbackTimer.value) {
+    return true;
+  }
+  return false;
+}
+function pause(pauseVal: number) {
+  if (pauseVal > 0) {
+    return SpotifyService.pause().pipe(delay(pauseVal * 1000));
   } else {
     return of('');
   }
 }
-function update(fromButton = false) {
-  let scrub = scrubber.value;
+function update(
+  scrubberVal: number,
+  playbackVal: number,
+  pauseVal: number,
+  fromButton = false
+) {
+  let scrub = scrubberVal;
 
-  console.log(scrubber.value, playbackTimer.value);
-  console.log(scrubber.value + playbackTimer.value);
-  if (
-    parseInt(scrubber.value as any) + parseInt(playbackTimer.value as any) ===
-    0
-  ) {
+  console.log(
+    `Scrubber ${scrubberVal}, Playback ${playbackVal}, Pause ${pauseVal}`
+  );
+
+  if (parseInt(scrubberVal as any) + parseInt(playbackVal as any) === 0) {
     console.info(
       'Scrubber and playback will cancel out, adjusting by 5 seconds.'
     );
@@ -58,10 +92,10 @@ function update(fromButton = false) {
     playbackSub.unsubscribe();
   }
 
-  playbackSub = timer(playbackTimer.value * 1000)
+  playbackSub = timer(playbackVal * 1000)
     .pipe(
       switchMap(() =>
-        pause().pipe(
+        pause(pauseVal).pipe(
           switchMap(() =>
             SpotifyService.fetchQueue().pipe(
               map((response) => response.queue.at(0))
@@ -74,7 +108,7 @@ function update(fromButton = false) {
             if (scrub > 0) {
               return SpotifyService.scrubToSeconds(scrub);
             } else if (scrub < 0 && nextSong) {
-              console.info('Going to last ' + Math.abs(scrub));
+              console.info('Going to last ' + Math.abs(scrub) + ' seconds');
 
               return SpotifyService.scrubToSeconds(
                 Math.round(nextSong.duration_ms / 1000) - Math.abs(scrub)
@@ -89,9 +123,9 @@ function update(fromButton = false) {
     .subscribe(() => {
       SnackbarService.msgSuccess(
         'Playback Updated',
-        `Skipped song, and scrubbed to ${scrubber.value} seconds`
+        `Skipped song, and scrubbed to ${scrubberVal} seconds`
       );
-      update();
+      update(scrubberVal, playbackVal, pauseVal);
     });
 
   if (fromButton) {
